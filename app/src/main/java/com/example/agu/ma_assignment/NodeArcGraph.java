4cd275633e526682600ad32eeeb28c402694dbb0
Agustin <andyova97@gmail.com>
2019-02-21 18:51:48 +0000

    Retrieving officers data
    Work more on node graph and displaying the data

--- app/src/main/java/com/example/agu/ma_assignment/NodeArcGraph.java
+++ app/src/main/java/com/example/agu/ma_assignment/NodeArcGraph.java
@@ -16,9 +16,32 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class NodeArcGraph extends AppCompatActivity {
 
     NodeArcGenerator v;
+    private String CName;
+    private int Cnumber;
+
+    private ArrayList<String> officerName = new ArrayList<>();
+    private ArrayList<String> officerDoB = new ArrayList<>();
+    private ArrayList<String> officerNat = new ArrayList<>();
+    private ArrayList<String> officerAddress = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +54,10 @@ public class NodeArcGraph extends AppCompatActivity {
 
         // Set back button in title bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //get officers
+        api_search_officers(v);
+
     }
 
 
@@ -41,11 +68,107 @@ public class NodeArcGraph extends AppCompatActivity {
         return true;
     }
 
+    //get comp name and number the user clicked on
     private void getIntentData(){
         if(getIntent().hasExtra("compName") && getIntent().hasExtra("compNumber")){
-            Log.d("intentExtra", "getIntentData: "+getIntent().getStringExtra("compName"));
-            Log.d("intentExtra", "getIntentData: "+getIntent().getStringExtra("compNumber"));
+            CName = getIntent().getStringExtra("compName");
+            Cnumber = Integer.parseInt(getIntent().getStringExtra("compNumber"));
+            Log.i("CompData", "Name; "+ CName + " Number; " + Cnumber);
+
         }
+        else{
+            Log.d("IntentExtras", "getIntentData: EMPTY");
+        }
+    }
+
+    private void api_search_officers(View view){
+        //Create request queue
+        RequestQueue rQueue = Volley.newRequestQueue(this);
+        //JSONRequest
+        //search url for company names
+        String searchURL = "https://api.companieshouse.gov.uk/company/" + Cnumber + "/officers?register_type=directors";
+        JsonObjectRequest JSONretriever = new JsonObjectRequest(Request.Method.GET, searchURL, null,
+                new Response.Listener < JSONObject > () {
+                    @Override
+                    public void onResponse(JSONObject response) { //what to do with the response from the API
+
+                        try {
+                            JSONArray arr = response.getJSONArray("items");
+                            for (int i = 0; i < arr.length(); i++) {
+                                JSONObject o = arr.getJSONObject(i);
+                                //add officer data to relevant array
+                                String name = o.getString("name");
+                                officerName.add(name);
+                                Log.i("logging data", "onResponse: " + name);
+
+                                //officer's nationality
+                                String nationality;
+                                if(!o.has("nationality")){
+                                    nationality = "Unavailable";
+                                    officerNat.add(nationality);
+                                }
+                                else{
+                                    nationality = o.getString("nationality");
+                                    officerNat.add(nationality);
+                                }
+                                Log.i("logging data", "onResponse: " + nationality);
+
+                                //officer's year of birth
+                                JSONObject dob = o.getJSONObject("date_of_birth");
+                                String yr;
+                                if(!dob.has("year")){
+                                    yr = "Unavailable";
+                                    officerDoB.add(yr);
+                                }
+                                else {
+                                    yr = dob.getString("year");
+                                    officerDoB.add(yr);
+                                }
+                                Log.i("logging data", "onResponse: " + yr);
+
+                                //officer's address
+                                JSONObject addr = o.getJSONObject("address");
+                                String postCode, cty, stNo, street, ctry, ofAddress;
+                                if(!addr.has("postal_code") || !addr.has("locality") || !addr.has("premises") || !addr.has("address_line_1") || !addr.has("country")){
+                                    ofAddress = "Unavailable";
+                                    officerAddress.add(ofAddress);
+
+                                }
+                                else {
+                                    postCode = addr.getString("postal_code");
+                                    cty = addr.getString("locality");
+                                    stNo = addr.getString("premises");
+                                    street = addr.getString("address_line_1");
+                                    ctry = addr.getString("country");
+                                    ofAddress = stNo + " " + street + ", " + cty + ", " + postCode + ". " + ctry;
+                                    officerAddress.add(ofAddress);
+                                }
+                                Log.i("logging data", "onResponse: " + ofAddress);
+                            }
+                        } catch (Exception e) { //if the response generates an exception, print it with printStackTrace()
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() { // if anything goes wrong... Print error to log
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        NetworkResponse networkResponse = error.networkResponse;
+                        Log.e("JSON", "JSON error: " + networkResponse.statusCode);
+                        //                        if (networkResponse != null && networkResponse.statusCode == HttpStatus.SC_UNAUTHORIZED) {
+//                            // HTTP Status Code: 401 Unauthorized
+//                        }
+                    }
+                }) {
+            @Override
+            public Map< String, String > getHeaders() throws AuthFailureError { // set the header for authentication (aka send API key)
+                HashMap headers = new HashMap();
+                headers.put("Authorization", "sic7yu4YhmpLToiLXPT7bGS_QvXW8SjIPLuhIfoF");
+                return headers;
+            }
+        };
+        //Finally, add the request to the request queue
+        rQueue.add(JSONretriever);
     }
 
 }
--- app/src/main/java/com/example/agu/ma_assignment/SearchActivity.java
+++ app/src/main/java/com/example/agu/ma_assignment/SearchActivity.java
@@ -61,45 +61,32 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     // Function used to retrieve all company names that suit the search
-    public void api_search_all(View view, String searchURL, final String type){
+    public void api_search_all(View view) {
         //Create request queue
         RequestQueue rQueue = Volley.newRequestQueue(this);
-        //Create the actual request (JsonObjecRequest)
-        Log.d("compnamesearch", "api_search_all: creating request...");
         //JSONRequest
-        JsonObjectRequest JSONretriever = new JsonObjectRequest(Request.Method.GET, searchURL,null,
-                new Response.Listener<JSONObject>() {
+        //search url for company names
+        searchURL = "https://api.companieshouse.gov.uk/search/companies?q=" + userSearch.getText().toString();
+
+        JsonObjectRequest JSONretriever = new JsonObjectRequest(Request.Method.GET, searchURL, null,
+                new Response.Listener < JSONObject > () {
                     @Override
                     public void onResponse(JSONObject response) { //what to do with the response from the API
-                        if(type == "search_companies"){
-                            try {
-                                CompanyNames.clear();
-                                CompanyNumber.clear();
-                                JSONArray arr = response.getJSONArray("items");
-                                for(int i=0;i<arr.length();i++) {
-                                    JSONObject o = arr.getJSONObject(i);
-                                    String name = (o.getString("title")).toString(); //get company name
-                                    String number = (o.getString("company_number")).toString(); // get company number
-                                    //String address = (o.getString("address_snippet")).toString(); // get company address in one line
-                                    CompanyNames.add(i, name); //add name of the company to the array
-                                    CompanyNumber.add(i, number); //add number of the company to the array
-                                    initRecycler();
-
-                                }
+                        try {
+                            CompanyNames.clear();
+                            CompanyNumber.clear();
+                            JSONArray arr = response.getJSONArray("items");
+                            for (int i = 0; i < arr.length(); i++) {
+                                JSONObject o = arr.getJSONObject(i);
+                                String name = (o.getString("title")).toString(); //get company name
+                                String number = (o.getString("company_number")).toString(); // get company number
+                                //String address = (o.getString("address_snippet")).toString(); // get company address in one line
+                                CompanyNames.add(i, name); //add name of the company to the array
+                                CompanyNumber.add(i, number); //add number of the company to the array
+                                initRecycler();
                             }
-                            catch (Exception e) {  //if the response generates an exception, print it with printStackTrace()
-                                e.printStackTrace();
-                            }
-                        }
-                        if(type == "search_officers"){
-                            try{
-                                CompanyNames.clear();
-                                CompanyNumber.clear();
-                                //add code to search and retrieve data from officers (list)
-                            }
-                            catch (Exception e){
-                                Log.e("search_officers", "onResponse: ",e );
-                            }
+                        } catch (Exception e) { //if the response generates an exception, print it with printStackTrace()
+                            e.printStackTrace();
                         }
                     }
                 },
@@ -108,19 +95,16 @@ public class SearchActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         Log.e("JSON", "JSON error: " + error.toString());
                     }
-                } ){
+                }) {
             @Override
-            public Map<String, String> getHeaders() throws AuthFailureError { // set the header for authentication (aka send API key)
+            public Map < String, String > getHeaders() throws AuthFailureError { // set the header for authentication (aka send API key)
                 HashMap headers = new HashMap();
                 headers.put("Authorization", "sic7yu4YhmpLToiLXPT7bGS_QvXW8SjIPLuhIfoF");
                 return headers;
             }
-
         };
-
         //Finally, add the request to the request queue
         rQueue.add(JSONretriever);
-
     }
 
     private void initRecycler(){
@@ -133,8 +117,7 @@ public class SearchActivity extends AppCompatActivity {
 
     public void searchComp(View view) {
         try{
-            searchURL = "https://api.companieshouse.gov.uk/search/companies?q=" + userSearch.getText().toString();
-            api_search_all(view, searchURL, "search_companies");
+            api_search_all(view);
         }
         catch (Exception e){
             Log.e("Error", "searchComp: " + e.getMessage());
